e75b30a6024f32cc2e582a308b7190bc8829f05b
samer khamisi <samer.khamisi@appliedis.com>
2021-10-08 23:31:41 +0000

    Initial Commit

---
+++ MathGames/Math Games.py
@@ -0,0 +1,38 @@
+#Application
+#-----------
+#Simple math test algorithm. Allows for addition, multiplication, and division questions
+#Allows for custom number of questions and will give user a score on a scale of 0-100.
+
+#Requirements
+#------------
+#1) Take user input for one of 4 categories to choose from : Addition, subtraction, multiplication, and division
+#2) Allow user input for custom number of problems
+#3) Build functions for addition, subtraction, multiplication, and division arithmetic
+#4) Provide generation for random set of numbers between a set interval
+#5) Build function for displaying correct or incorrect answer to user
+#6) Keep track of correct/incorrect answers to provide user with a score
+
+import mathFunctions as mf
+
+print('Welcome to Math Games!')
+print('======================')
+print('''
+Enter [1] for addition
+Enter [2] for subtraction
+Enter [3] for multiplication 
+Enter [4] for division''')
+userInput = int(input('\nChoose a math category: '))
+userInput = mf.checkValidInput(userInput)
+if userInput == 1:
+    userChoice = int(input('\nHow many problems would you like to solve? '))
+    mf.RunAddition(userChoice)
+if userInput == 2:
+    userChoice = int(input('\nHow many problems would you like to solve? '))
+    mf.RunSubtraction(userChoice)
+if userInput == 3:
+    userChoice = int(input('\nHow many problems would you like to solve? '))
+    mf.RunMultiplication(userChoice)
+if userInput == 4:
+    userChoice = int(input('\nHow many problems would you like to solve? '))
+    mf.RunDivision(userChoice)
+
