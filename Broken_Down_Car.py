be6ff14d201fa8f8e1b8f01b26b9610d073ad4da
samer khamisi <samer.khamisi@appliedis.com>
2021-11-05 13:36:02 -0500

    Added new method

--- Broken_Down_Car.py
+++ Broken_Down_Car.py
@@ -16,21 +16,32 @@ monthlyNewCarPayment = round(newCarPrice / 12) + (newCarPrice * .10)
 usedCarPrice = 9000
 monthlyUsedCarPayment = round(usedCarPrice / 12) + (usedCarPrice * .12)
 
+#Runs function for reapiring current car
 def repairCar():
+    userChoice = input("You've chosen to repair your car, are you sure about that?")
+    if userChoice == 'yes' or userChoice == 'Yes':
+        print(f'You make: {monthlySalary} every month')
+        print(f'After paying your current bills, you will have {takeHomeSalary}')
+        print(f'Repairing your car will cost {repairCost} and add {monthlyRepairPayment} to your monthly bills')
+        print(f'If you repair your car, you will have {takeHomeSalary - monthlyRepairPayment}')
+#Run function for buying a new car
 def buyNewCar():
+
+#Runs function for buying a used car
 def buyUsedCar():
 
+#Presents user with three scenarios concerning their broken car
 def makeAChoice():
     userChoice = input('''Make a selection
     Enter [1] to repair your car
     Enter [2] to buy a new car
     Enter [3]to buy a used car ''')
     if userChoice == 1:
-    repairCar()
-     if userChoice == 2:
-    buyNewCar()
-      if userChoice == 3:
-    buyUsedCar()
+        repairCar()
+    if userChoice == 2:
+        buyNewCar()
+    if userChoice == 3:
+        buyUsedCar()
 
 makeAChoice()
 
