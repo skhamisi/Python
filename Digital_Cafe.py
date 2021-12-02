3e285e70eb8eb811c3852b368695658b3710c3f7
samer khamisi <samer.khamisi@appliedis.com>
2021-12-02 11:09:41 -0600

    '

--- Digital_Cafe.py
+++ Digital_Cafe.py
@@ -1,13 +1,22 @@
 #Digital Cafe
 import os
+
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
+class colors:
+    header = '\u001b[34m' #BLUE #BOLD #UNDERLINE
+    assistant = '\033[93m' #YELLOW
+    warning = '\033[91m' #RED
+    reset = '\033[0m' #RESET COLOR
+
 cls()
 
-print("\nWelcome to the Digital Cafe, what can I get started for you?")
+print(f'''{colors.header}Welcome to the Digital Cafe
+---------------------------\n''')
 
-print('''Enter your order below
+print(f"{colors.assistant}Hi, my name is Aey-I, I'll be your sever today. What can I get started for you?\n")
+print(f'''{colors.reset}Enter your order below
 *NOTE: Please put a comma in between each item\n''')
 
 userBreakfast = input().lower()
@@ -15,33 +24,33 @@ userBreakfast = input().lower()
 breakfast = userBreakfast.split(', ')
 
 if 'egg' in breakfast or 'eggs' in breakfast:
-    eggStyle = input('\nHow would you like your eggs? ')
-    print(f"\nAttendant: {eggStyle}, got it, good choice\n")
+    eggStyle = input(f'{colors.assistant}\nAey-I: How would you like your eggs? ')
+    print(f"\nAey-I: {eggStyle}, got it, good choice\n")
 if 'coffee' in breakfast:
-    coffeeChoice = input('Attendant: Would you like cream in your coffee? ').lower()
+    coffeeChoice = input('Aey-I: Would you like cream in your coffee? ').lower()
     if 'yes' in coffeeChoice:
-        print('\nAttendant: One coffee with cream coming right up')
+        print('\nAey-I: One coffee with cream coming right up')
     else:
-        print('\nAttendant: Black coffee it is\n')
+        print('\nAey-I: Black coffee it is\n')
 
-userResponse = input('\nAttendant: Can I get anything else for you? ').lower()
+userResponse = input('\Aey-I: Can I get anything else for you? ').lower()
 # if 'yes' in userResponse:
 #     extraOrder = input('\nAttendant: What would you like? ').lower()
 #     breakfast.append(extraOrder)
 
 while 'yes' in userResponse:
-    extraOrder = input('\nAttendant: What would you like? ').lower()
+    extraOrder = input('\Aey-I: What would you like? ').lower()
     breakfast.append(extraOrder)
-    userResponse = input('\nAttendant: Can I get anything else for you? ').lower()
+    userResponse = input('\Aey-I: Can I get anything else for you? ').lower()
     
 
-print('\nAttendant: I got your order as')
+print('\Aey-I: I got your order as')
 print(*breakfast, sep = "\n")
 
-userReply = input('\nAttendant: Did I miss anything? ').lower()
+userReply = input('\Aey-I: Did I miss anything? ').lower()
 if 'yes' in userReply:
-    forgottenOrder = input('\nAttendant: What did I miss? ').lower()
+    forgottenOrder = input('\Aey-I: What did I miss? ').lower()
     breakfast.append(forgottenOrder)
-    print(f'Attendant: Sorry about that, I added {forgottenOrder} to your order')
+    print(f'Aey-I: Sorry about that, I added {forgottenOrder} to your order')
 
-print("\nAttendant: Thanks! we'll have your order out right away")
+print("\Aey-I: Thanks! we'll have your order out right away")
