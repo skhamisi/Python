3c085c7230b3ce6e41c4dfcdd28df226413def16
samer khamisi <samer.khamisi@appliedis.com>
2021-12-09 09:42:32 -0600

    '

--- Digital_Cafe/Digital_Cafe.py
+++ Digital_Cafe/Digital_Cafe.py
@@ -1,37 +1,40 @@
 #Digital Cafe
-import os
+import os, Digital_Cafe_Func as dcf
+
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
-class colors:
-    header = '\u001b[34m' #BLUE
-    assistant = '\033[93m' #YELLOW
-    warning = '\033[91m' #RED
-    reset = '\033[0m' #RESET COLOR
+# class colors:
+#     header = '\u001b[34m' #BLUE
+#     assistant = '\033[93m' #YELLOW
+#     warning = '\033[91m' #RED
+#     reset = '\033[0m' #RESET COLOR
 
 cls()
 
-print(f'''{colors.header}Welcome to the Digital Cafe
+print(f'''{dcf.colors.header}Welcome to the Digital Cafe
 ---------------------------\n''')
 
-print(f"{colors.assistant}Hi, my name is Aey-I, I'll be your sever today. What can I get started for you?\n")
-print(f'''{colors.reset}Enter your order below
+print(f"{dcf.colors.assistant}Hi, my name is Aey-I, I'll be your sever today. What can I get started for you?\n")
+print(f'''{dcf.colors.reset}Enter your order below
 *NOTE: Please put a comma in between each item\n''')
 
 userBreakfast = input().lower()
 
 breakfast = userBreakfast.split(', ')
 
-if 'egg' in breakfast or 'eggs' in breakfast:
-    eggStyle = input(f'{colors.assistant}\nAey-I: How would you like your eggs? ')
-    print(f"\nAey-I: {eggStyle}, got it, good choice\n")
-if 'coffee' in breakfast:
-    coffeeChoice = input('Aey-I: Would you like cream in your coffee? ').lower()
-    if 'yes' in coffeeChoice:
-        print('\nAey-I: One coffee with cream coming right up')
-    else:
-        print('\nAey-I: Black coffee it is\n')
+dcf.itemStyle(breakfast)
+# def itemStyle():
+#     if 'egg' in breakfast or 'eggs' in breakfast:
+#         eggStyle = input(f'{colors.assistant}\nAey-I: How would you like your eggs? ')
+#         print(f"\nAey-I: {eggStyle}, got it, good choice\n")
+#     if 'coffee' in breakfast:
+#         coffeeChoice = input('Aey-I: Would you like cream in your coffee? ').lower()
+#         if 'yes' in coffeeChoice:
+#             print('\nAey-I: One coffee with cream coming right up')
+#         else:
+#             print('\nAey-I: Black coffee it is\n')
 
 userResponse = input('\Aey-I: Can I get anything else for you? ').lower()
 # if 'yes' in userResponse:
---
+++ Digital_Cafe/Digital_Cafe_Func.py
@@ -0,0 +1,17 @@
+class colors:
+    header = '\u001b[34m' #BLUE
+    assistant = '\033[93m' #YELLOW
+    warning = '\033[91m' #RED
+    reset = '\033[0m' #RESET COLOR
+
+def itemStyle(breakfast):
+    if 'egg' in breakfast or 'eggs' in breakfast:
+        eggStyle = input(f'{colors.assistant}\nAey-I: How would you like your eggs? ')
+        print(f"\nAey-I: {eggStyle}, got it, good choice\n")
+    if 'coffee' in breakfast:
+        coffeeChoice = input('Aey-I: Would you like cream in your coffee? ').lower()
+        if 'yes' in coffeeChoice:
+            print('\nAey-I: One coffee with cream coming right up')
+        else:
+            print('\nAey-I: Black coffee it is\n')
+
